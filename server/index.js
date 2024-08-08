4a1891752fde82a7f6e43b6aa8b4381f6c16e18a
Shaurya Jha <shauryajha35@gmail.com>
2024-08-09 01:44:52 +0530

    indentation

--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
 //express config
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 
 //express parse
 const app = express();
 app.use(express.json());
 
 //routes
-const schedulerRoutes = require('./routes/schedulerRoutes.js');
+const schedulerRoutes = require("./routes/schedulerRoutes.js");
 
 //use routes
 app.use("/", schedulerRoutes);
 
 // Connect to MongoDB and start the server
 mongoose.connect(process.env.MONGO_URI).then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Connected to DB and running on port http://localhost:${process.env.PORT}/`);
-    });
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `Connected to DB and running on port http://localhost:${process.env.PORT}/`
+    );
   });
-  
+});
